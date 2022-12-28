f1f2571572207dda0cc8ddd2582acab847655262
Tianyu Gu <macdavid313@gmail.com>
2022-12-29 10:24:17 +1100

    update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,14 @@
 
 set -euxo pipefail
 
+CFLAGS="-g -Wall -I${UV_PATH}/include"
+
 # 2. uv-pickaxe
-clang -o tool/uv-pickaxe -I$HOME/.local/include -luv -L$HOME/.local/lib tool/uv-pickaxe.c
+# FIXME: it only works on linux now
+cc ${CFLAGS} -c tool/uv-pickaxe.c  -o tool/uv-pickaxe.o
+cc tool/uv-pickaxe.o -L${UV_PATH}/lib -luv -o tool/uv-pickaxe
 ./tool/uv-pickaxe src/uv-constants.cl
+rm tool/*.o
 
 # 3. compile, load, and combine fasls
 ${LISP} -q -W -L load.cl --kill
